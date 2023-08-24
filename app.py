0233f0fcc210227740daceb7bd9e67f27bef1ac5
Maksim Samoshin <vip.samoshin@mail.ru>
2023-08-24 18:13:17 +0300

    Исправления + новый нерабочий тест

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from selene import be
-from selene.support.shared import browser
 
 
 def given_opened():
     if browser.element('#com.dma.author.connect:id/touch_outside').matching(be.visible):
-        browser.element('#com.dma.author.connect:id/close_button').click()
+        browser.element('#com.dma.author.connect:id/close_button')
--- test_appium.py
+++ test_appium.py
@@ -1,22 +1,27 @@
-import unittest
-from appium import webdriver
-from appium_patch import monkey
-webdriver.Remote = monkey.patch_all()
-from selene.support.shared import browser
-import app
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
+from selene import browser
+
 desired_caps = dict(
     platformName='Android',
     platformVersion='8.0',
     automationName='uiautomator2',
     deviceName='pixel_3a_xl',
-    app='C:\Users\artyo\Desktop\com.dma.author.android.x.connect-v1.0.0(3)-author-google-debug'
+    app=r'C:\Users\artyo\Desktop\x-author-google-debug.apk'
 )
 
-driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+def test_first():
+    android_options = UiAutomator2Options()
+    android_options.new_command_timeout = 15
+    browser.config.driver_options = android_options
 
+    by_id = lambda id: (AppiumBy.ID, f'com.dma.author.android.x.connect:id/{id}')
 
-def test_autorisation():
+    browser.config.driver = driver.Remote(
+        command_executor='http://127.0.0.1:4723/wd/hub',
+        options=android_options,
+        )
 
-    app.given_opened()
-    browser.element('#com.dma.author.connect:id/close_button').click()
-    browser.element('#com.dma.author.connect:id/textinput_placeholder').click().type(79819740196)
+    browser.element(by_id('continue_button')).click()
+    browser.element((AppiumBy.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/androidx.appcompat.widget.LinearLayoutCompat[1]/androidx.appcompat.widget.LinearLayoutCompat/androidx.appcompat.widget.LinearLayoutCompat/android.widget.EditText')).click().type('123@mail.ru')
+
